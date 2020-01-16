451726d7a8637224434c21759a36af88584777f4
abraaonma <abraaonmazevedo@gmail.com>
2020-01-15 21:38:12 -0300

    Menu de plano de negocios

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<title>Plano de Negócios</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="">SUMÁRIO EXECUTIVO</a>
+				<ul>
+					<li><a href="">Resumo</a></li>
+					<li><a href="">Dados dos Sócios</a></li>
+					<li><a href="">Missão, Visão e Valores</a></li>
+					<li><a href="">Setor do Negócio</a></li>
+					<li><a href="">Forma Jurídica</a></li>
+					<li><a href="">Enquadramento Tributário</a></li>
+					<li><a href="">Capital Social e Contrato</a></li>
+					<li><a href="">Fonte de Recursos</a></li>
+				</ul>
+			</li>
+			<li><a href="">ANÁLISE DE MERCADO</a>
+				<ul>
+					<li><a href="">Perfil de Consumidor</a></li>
+					<li><a href="">Diagnóstico da Concorrência</a></li>
+					<li><a href="">Dados do Fornecedor</a></li>
+				</ul>
+			</li>
+			<li><a href="">PLANO DE MARKETING</a>
+				<ul>
+					<li><a href="">Produtos e Serviços</a></li>
+					<li><a href="">Preços</a></li>
+					<li><a href="">Promoção e Política de Vendas</a></li>
+					<li><a href="">Canais de Comercialização</a></li>
+					<li><a href="">Localização do negócio</a></li>
+				</ul>
+			</li>
+			<li><a href="">PLANO OPERACIONAL</a>
+				<ul>
+					<li><a href="">Estrtura física e Layout</a></li>
+					<li><a href="">Capacidade de Produção</a></li>
+					<li><a href="">Processo Operacional e Fluxos</a></li>
+					<li><a href="">Cargo e Qualificação</a></li>
+					<li><a href="">Localização do negócio</a></li>
+				</ul>
+			</li>
+			<li><a href="">PLANO FINANCEIRO</a>
+				<ul>
+					<li><a href="">Equipamentos e Materiais</a></li>
+					<li><a href="">Capital de Giro (Estoque)</a></li>
+					<li><a href="">Capital de Giro (Caixa)</a></li>
+					<li><a href="">Investimentos Operacionais</a></li>
+					<li><a href="">Investimento Total</a></li>
+					<li><a href="">Faturamento Mensal</a></li>
+					<li><a href="">Custo Unitário</a></li>
+					<li><a href="">Custo de Comercialização</a></li>
+					<li><a href="">Custo de Mercadoria Vendida (MD/MV)</a></li>
+					<li><a href="">Custo de Mão de Obra</a></li>
+					<li><a href="">Custo de Depreciação Total</a></li>
+					<li><a href="">Custo Fixo Mensal</a></li>
+					<li><a href="">Demonstrativo de Resuldo do Exercício (DRE)</a></li>
+					<li><a href="">Indicadores de Viabilidade</a></li>
+				</ul>
+			</li>
+			<li><a href="">CENÁRIO ORÇAMENTÁRIO</a>
+				<ul>
+					<li><a href="">Previsão de Receita e Ações Preventivas e Corretivas</a></li>
+				</ul>
+			</li>
+			<li><a href="">DIAGNÓSTICO ESTRATÉGICO</a>
+				<ul>
+					<li><a href="">Análise da Matriz SWOT</a></li>
+				</ul>
+			</li>
+			<li><a href="">AVALIAÇÃO DO PLANO DE NEGÓCIOS</a>
+				<ul>
+					<li><a href="">Método de Avaliação</a></li>
+					<li><a href="">Descrição de Tendências</a></li>
+					<li><a href="">Gestão de Risco</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</body>
+</html>
